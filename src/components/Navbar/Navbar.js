d8bdf859d39818666703d851a4b6c59e2fe64aa8
JesusM15 <guerrajesusm72@gmail.com>
2023-08-28 17:01:46 -0700

    Icons download

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,17 @@
 import classes from './Navbar.module.css';
 
 const Navbar = () =>{
-    
+    return(
+        <nav className="flex max-h-12 justify-end">
+            <ul className="">
+                <li>
+                    <a href="#" target="_blank">
+                        
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
 };
 
 export default Navbar;
