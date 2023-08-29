e465ac12c68beb84587c659f40c62612434f709a
Jesus <guerrajesusm72@gmail.com>
2023-08-29 16:11:27 -0700

    only linkedin

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,13 @@
 import classes from './Navbar.module.css';
-import { BsLinkedin, BsDiscord} from "react-icons/bs";
+import { BsLinkedin}  from "react-icons/bs";
 
 const Navbar = () =>{
     return(
-        <nav className="flex max-h-16 justify-end p-2 absolute w-full">
+        <nav className="flex max-h-16 justify-end py-2 px-4 absolute w-full">
             <ul className="flex flex-row gap-4">
                 <li>
                     <a href="#" target="_blank">
                        <BsLinkedin  className={`text-3xl ${classes.icon}`}/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" target="_blank">
-                       <BsDiscord  className={`text-3xl ${classes.icon}`}/>
                     </a>
                 </li>
             </ul>
